275091df2522715668345829391a521d6c392648
AlexT97x <alextinella@gmail.com>
2023-05-22 10:21:58 +0200

    implementazione primo bonus

--- script/script.js
+++ script/script.js
@@ -1,29 +1,30 @@
 "use strict";
 
+
+const output = document.getElementById('output');
+
 for(let i = 1; i <= 100; i++){
 
 
     if (i % 15 == 0) {
 
-        console.log('Fizz Buzz')
+        output.innerHTML = '<div>Fizz Buzz</div>';
+        console.log('Fizz Buzz');
 
     }else if (i % 5 == 0) {
 
+        output.innerHTML = '<div>Buzz</div>';
         console.log('buzz');
 
     }else if (i % 3 == 0) {
 
-
-
+        output.innerHTML = '<div>Fizz</div>';
         console.log('Fizz');
-
 
     }else {
 
-
         console.log(i);
+        output.innerHTML = '<div>${i}</div>'
     }
-
-
 
 }
